bb75f235e4e8fb7e51ee4d1ccff93fa3932a6cd9
Intengine <studio@intengine.pl>
2018-08-21 14:49:59 +0200

    Main class 'Pong'
    Added main class and Renderer, which will provide bars and ball.

---
+++ src/Pong.java
@@ -0,0 +1,11 @@
+public class Pong {
+    public static Pong pong;
+
+    public Pong() {
+
+    }
+
+    public static void main(String[] args) {
+        pong = new Pong();
+    }
+}
---
+++ src/Renderer.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class Renderer extends JPanel{
+    private static final long serialVersionUID = 1L;
+}
