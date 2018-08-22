bb929894676ebbb0cec1950768579580bd68d937
Intengine <studio@intengine.pl>
2018-08-22 10:59:29 +0200

    Add controls
    Added key listeners to control both bars.

--- src/Paddle.java
+++ src/Paddle.java
@@ -6,8 +6,8 @@ public class Paddle {
     public int x;
     public int y;
 
-    public int width = 100;
-    public int height = 500;
+    public int width = 50;
+    public int height = 300;
 
     public int score;
 
@@ -15,7 +15,7 @@ public class Paddle {
         this.paddleNumber = paddleNumber;
 
         if(paddleNumber == 1) {
-            this.x = width;
+            this.x = 0;
         }
 
         if(paddleNumber == 2) {
@@ -28,5 +28,21 @@ public class Paddle {
     public void render(Graphics g) {
         g.setColor(Color.WHITE);
         g.fillRect(x, y, width, height);
+    }
+
+    public void move(boolean up) {
+        int speed = 5;
+
+        if(up) {
+            if(y + height - speed > Pong.pong.height) {
+                y--;
+            } else {
+                y = 0;
+            }
+        } else {
+            if(y + speed < Pong.pong.height) {
+                y++;
+            }
+        }
     }
 }
--- src/Pong.java
+++ src/Pong.java
@@ -2,13 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Pong implements ActionListener {
+public class Pong implements ActionListener, KeyListener {
     public static Pong pong;
     public Renderer renderer;
 
     public Paddle player1;
     public Paddle player2;
+
+    public boolean bot = false;
+    public boolean w, s, up, down;
 
     public int width = 700;
     public int height = 700;
@@ -19,10 +24,11 @@ public class Pong implements ActionListener {
         JFrame jframe = new JFrame("Pong");
         renderer = new Renderer();
 
-        jframe.setSize(width, height);
+        jframe.setSize(width + 15, height);
         jframe.setVisible(true);
         jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
         jframe.add(renderer);
+        jframe.addKeyListener(this);
 
         start();
 
@@ -35,6 +41,21 @@ public class Pong implements ActionListener {
     }
 
     public void update() {
+        if(w) {
+            player1.move(true);
+        }
+
+        if(s) {
+            player1.move(false);
+        }
+
+        if(up) {
+            player2.move(true);
+        }
+
+        if(down) {
+            player2.move(false);
+        }
     }
 
     public void render(Graphics g) {
@@ -53,5 +74,51 @@ public class Pong implements ActionListener {
 
     public static void main(String[] args) {
         pong = new Pong();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int id = e.getKeyCode();
+
+        if(id == KeyEvent.VK_W) {
+            w = true;
+        }
+
+        if(id == KeyEvent.VK_S) {
+            s = true;
+        }
+
+        if(id == KeyEvent.VK_UP) {
+            up = true;
+        }
+
+        if(id == KeyEvent.VK_DOWN) {
+            down = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int id = e.getKeyCode();
+
+        if(id == KeyEvent.VK_W) {
+            w = false;
+        }
+
+        if(id == KeyEvent.VK_S) {
+            s = false;
+        }
+
+        if(id == KeyEvent.VK_UP) {
+            up = false;
+        }
+
+        if(id == KeyEvent.VK_DOWN) {
+            down = false;
+        }
     }
 }
